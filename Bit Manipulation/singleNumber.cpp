d025f5ee57faadc084451c52cf53722694a911a2
ayushpratap27 <ayushpratap2127@gmail.com>
2023-11-23 23:42:39 +0530

    Created singleNumber.cpp

---
+++ Bit Manipulation/singleNumber.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int singleNumber(const std::vector<int>& nums) {
+    int result = 0;
+
+    // XOR all elements in the array
+    for (int num : nums) {
+        result ^= num;
+    }
+
+    return result;
+}
