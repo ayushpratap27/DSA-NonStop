cefb9eaba713671453d05b7f34d7ae7856a93cdf
ayushpratap27 <ayushpratap2127@gmail.com>
2023-11-29 17:16:24 +0530

    initial commit

---
+++ Bit Manipulation/oddEven.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string oddEven(int N){
+    // Write your code here.
+    string str1="odd",str2="even";
+    if(N%2!=0){
+        return str1;
+    }else{
+        return str2;
+    }
+}
