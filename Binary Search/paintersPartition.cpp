6db88f4e13818963a08dccf321bb4099630e4b1b
ayushpratap27 <ayushpratap2127@gmail.com>
2023-12-11 21:44:09 +0530

    Created paintersPartition.cpp

---
+++ Binary Search/paintersPartition.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countStudents(vector<int> &arr,int pages){
+    int students=1;
+    long long pagesStudent=0;
+    for(int i=0;i<arr.size();i++){
+        if(pagesStudent+arr[i]<=pages){
+            pagesStudent+=arr[i];
+        }else{
+            students+=1;
+            pagesStudent=arr[i];
+        }
+    }
+    return students;
+}
+
+int findPages(vector<int>& arr, int n, int m) {
+    // Write your code here.
+    if(m>n) return -1;
+    int low=*max_element(arr.begin(),arr.end());
+    int high=accumulate(arr.begin(),arr.end(),0);
+    while(low<=high){
+        int mid =(low+high)/2;
+        int students=countStudents(arr,mid);
+        if(students>m){
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    return low;
+}
+// Time complexity= O(log(sum-max+1)*n)
+
+int findLargestMinDistance(vector<int> &boards, int k){
+    //    Write your code here.
+    return findPages(boards, boards.size(), k);
+}
