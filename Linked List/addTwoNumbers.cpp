c788a2f2b437b2130aa54b43fc802fa8efd70260
ayushpratap27 <ayushpratap2127@gmail.com>
2023-12-28 23:15:17 +0530

    Created addTwoNumbers.cpp

---
+++ Linked List/addTwoNumbers.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* current = dummy;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+
+        return dummy->next;
+    }
+};
+
+// Function to print a linked list
+void printList(ListNode* head) {
+    while (head) {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    // Example usage
+    ListNode* l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+    ListNode* l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+    Solution solution;
+    ListNode* result = solution.addTwoNumbers(l1, l2);
+
+    // Print the result
+    printList(result);
+
+    // Clean up memory
+    delete l1;
+    delete l2;
+    while (result) {
+        ListNode* temp = result;
+        result = result->next;
+        delete temp;
+    }
+
+    return 0;
+}
