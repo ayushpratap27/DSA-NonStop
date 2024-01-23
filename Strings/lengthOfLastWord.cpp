8921327758b20e7606f8fc2fa288624f1374c23a
ayushpratap27 <ayushpratap2127@gmail.com>
2024-01-23 22:28:53 +0530

    Create lengthOfLastWord.cpp

---
+++ Strings/lengthOfLastWord.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lengthOfLastWord(string s){
+        int length=0;
+        int n=s.size()-1;
+        while(n>=0 && s[n]==' '){
+            n--;
+        }  
+        
+        while(n>=0 && s[n]!=' '){
+            length++;
+            n--;
+        }
+
+        return length;
+        
+}
