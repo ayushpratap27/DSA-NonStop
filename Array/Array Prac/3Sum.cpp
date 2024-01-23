2c2bd2f6ef05c62d09e7c166d1740033d31bc475
ayushpratap27 <ayushpratap2127@gmail.com>
2024-01-23 22:49:49 +0530

    Update 3Sum.cpp

--- Array/Array Prac/3Sum.cpp
+++ Array/Array Prac/3Sum.cpp
@@ -1,37 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 vector<vector<int>> triplet(int n, vector<int> &arr) {
     vector<vector<int>> result;
-
-    // Sort the array to make it easier to find triplets
     sort(arr.begin(), arr.end());
 
-    for (int i = 0; i < n - 2; ++i) {
-        // Skip duplicate elements
-        if (i > 0 && arr[i] == arr[i - 1]) continue;
+    for(int i=0;i<n-2;i++){
+        
+        if(i>0 && arr[i]==arr[i-1]) continue;
 
-        int left = i + 1;
-        int right = n - 1;
+        int left = i+1;
+        int right = n-1;
 
-        while (left < right) {
-            int current_sum = arr[i] + arr[left] + arr[right];
+        while(left<right){
+            int current_sum = arr[i]+arr[left]+arr[right];
 
-            if (current_sum == 0) {
+            if(current_sum==0){
                 result.push_back({arr[i], arr[left], arr[right]});
 
                 // Skip duplicate elements
-                while (left < right && arr[left] == arr[left + 1]) left++;
-                while (left < right && arr[right] == arr[right - 1]) right--;
+                while(left<right && arr[left]==arr[left+1]) left++;
+                while(left<right && arr[right]==arr[right-1]) right--;
 
                 left++;
                 right--;
-            } else if (current_sum < 0) {
+            }else if(current_sum<0) {
                 left++;
-            } else {
+            }else{
                 right--;
             }
         }
