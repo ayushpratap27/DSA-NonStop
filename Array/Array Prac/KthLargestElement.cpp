2e905299c5de748e6b79d069c48e05628446b6a1
ayushpratap27 <ayushpratap2127@gmail.com>
2024-01-25 23:02:21 +0530

    Update KthLargestElement.cpp

--- Array/Array Prac/KthLargestElement.cpp
+++ Array/Array Prac/KthLargestElement.cpp
@@ -1,9 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int findKthLargest(vector<int>& nums, int k) {
+// int findKthLargest(vector<int>& nums, int k) {
         
-    sort(nums.begin(), nums.end());
+//     sort(nums.begin(), nums.end());
         
-    return nums[nums.size()-k];
+//     return nums[nums.size()-k];
+// }
+
+vector<int> Klargest(vector<int> &a, int k, int n) {
+    sort(a.begin(), a.end());
+
+    vector<int> result(a.end() - k, a.end());
+    return result;
 }
+
