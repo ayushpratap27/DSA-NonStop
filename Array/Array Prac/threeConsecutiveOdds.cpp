39ea2b6a42cd069c1fa7f7bbedf1e015a36c502d
ayushpratap27 <ayushpratap2127@gmail.com>
2024-01-27 22:49:47 +0530

    Create threeConsecutiveOdds.cpp

---
+++ Array/Array Prac/threeConsecutiveOdds.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool threeConsecutiveOdds(vector<int>& arr){
+    int n=arr.size();
+    if(n<3){
+        return false;
+    }
+    for(int i=0;i<n-2;i++){
+        if(arr[i]%2!=0 && (arr[i+1]%2!=0 && arr[i+2]%2!=0)){
+            return true;
+        }
+    }
+    return false;
+}
