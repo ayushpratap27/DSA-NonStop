7a2039099db399ba5b826717828dc39dddbd0ae3
ayushpratap27 <ayushpratap2127@gmail.com>
2024-02-02 23:39:38 +0530

    Create removeDuplicatesDLL.cpp

---
+++ Linked List/removeDuplicatesDLL.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+     int data;
+     Node *prev;
+     Node *next;
+     Node() {
+         this->data = 0;
+         this->prev = NULL;
+         this->next = NULL;
+     }
+     Node(int data) {
+         this->data = data;
+         this->prev = NULL;
+         this->next = NULL;
+     }
+     Node (int data, Node *next, Node *prev) {
+         this->data = data;
+         this->prev = prev;
+         this->next = next;
+     }
+};
+
+Node* removeDuplicates(Node* head){
+    if(!head){
+        return nullptr; 
+    }
+
+    Node* current=head;
+
+    while(current->next){
+        if(current->data==current->next->data) {
+            Node* duplicate=current->next;
+            current->next=duplicate->next;
+            if(duplicate->next){
+                duplicate->next->prev=current;
+            }
+            delete duplicate;
+        }else{
+            current = current->next;
+        }
+    }
+
+    return head;
+}
