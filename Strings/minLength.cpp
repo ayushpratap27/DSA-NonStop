097186863d556a6b5495525d604e844f5f70cbcb
ayushpratap27 <ayushpratap2127@gmail.com>
2024-02-08 00:41:35 +0530

    Create minLength.cpp

---
+++ Strings/minLength.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minLength(string s) {
+    int n=s.size();
+    stack<char> stck;
+    stck.push('#');
+    for(int i=0; i<n; i++){
+        stck.push(s[i]);
+
+        if(stck.top()=='B'){
+            stck.pop();
+            if(stck.top()=='A'){
+                stck.pop();
+            } 
+            else {
+                stck.push('B');
+            }
+        }
+        else if(stck.top()=='D'){
+            stck.pop();
+            if(stck.top()=='C'){
+                stck.pop();
+            } 
+            else {
+                stck.push('D');
+            }
+        }
+    }
+
+    return stck.size()-1;
+
+}
