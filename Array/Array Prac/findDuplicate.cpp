db37886b1c2154b8384bdc1d8169555e50d3c663
ayushpratap27 <ayushpratap2127@gmail.com>
2024-02-10 23:32:23 +0530

    Create findDuplicate.cpp

---
+++ Array/Array Prac/findDuplicate.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> findDuplicates(vector<int>& nums) {
+    vector<int> result;
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<nums.size()-1;i++){
+        if(nums[i]==nums[i+1]){
+            result.push_back(nums[i]);
+        }
+    }
+    return result;
+}
