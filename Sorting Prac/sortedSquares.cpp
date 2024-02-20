809b9388525c1eb4881f8c3dec5e0e43abace02f
ayushpratap27 <ayushpratap2127@gmail.com>
2024-02-20 21:43:00 +0530

    Created sortedSquares.cpp

---
+++ Sorting Prac/sortedSquares.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> sortedSquares(vector<int>& nums) {
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        int temp=nums[i];
+        nums[i]=temp*temp;
+    }
+    sort(nums.begin(),nums.end());
+
+    return nums;
+}
