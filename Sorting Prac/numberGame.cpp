d7f69c417631fb2701c69341b5b245063c907bf5
ayushpratap27 <ayushpratap2127@gmail.com>
2024-02-21 18:28:19 +0530

    Create numberGame.cpp

---
+++ Sorting Prac/numberGame.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> numberGame(vector<int>& nums) {
+    
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<nums.size()-1;i+=2){
+        swap(nums[i],nums[i+1]);
+    }
+
+    return nums;
+
+}
