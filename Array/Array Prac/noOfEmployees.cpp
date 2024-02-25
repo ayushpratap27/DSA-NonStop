a8cfc0dc0ca4e1dfdfa64020d5525c73c186a30c
ayushpratap27 <ayushpratap2127@gmail.com>
2024-02-25 23:12:55 +0530

    Create noOfEmployees.cpp

---
+++ Array/Array Prac/noOfEmployees.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+    int count = 0;
+    for(int i=0;i<hours.size();i++){
+        if(hours[i] >= target){
+            count++;
+        } 
+    }
+
+    return count;
+}
