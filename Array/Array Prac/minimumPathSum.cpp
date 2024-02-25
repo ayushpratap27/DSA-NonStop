9cd509009daf41101782a34a7296bb112a4adeea
ayushpratap27 <ayushpratap2127@gmail.com>
2024-02-25 23:12:33 +0530

    Create minimumPathSum.cpp

---
+++ Array/Array Prac/minimumPathSum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+int minimumPathSum(vector<vector<int>>& triangle, int n) {
+    for(int i=n-2;i>=0;i--){
+        for(int j=0;j<=i;j++){
+            triangle[i][j]+= min(triangle[i+1][j],triangle[i+1][j+1]);
+        }
+    }
+
+    return triangle[0][0];
+}
