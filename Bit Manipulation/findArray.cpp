ef4725f2e3301ef8449be5824cfcf902f02d882b
ayushpratap27 <ayushpratap2127@gmail.com>
2024-03-16 23:26:46 +0530

    Create findArray.cpp

---
+++ Bit Manipulation/findArray.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> findArray(vector<int>& pref) {
+    // time/space: O(n)/O(n)
+    // vector<int> arr(pref);
+    // for(int i=1;i<pref.size();i++){
+    //     arr[i]=pref[i-1]^pref[i];
+    // }
+
+    // return arr;
+
+    //time/space: O(n)/O(1)
+    for(int i=pref.size()-1;i>0;i--`){
+        pref[i]^=pref[i-1];
+    }
+    
+    return pref;
+
+}
