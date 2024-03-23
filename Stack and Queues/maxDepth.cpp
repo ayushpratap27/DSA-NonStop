25717e845307673efb51a0a374938212757b3755
ayushpratap27 <ayushpratap2127@gmail.com>
2024-03-23 22:46:03 +0530

    Create maxDepth.cpp

---
+++ Stack and Queues/maxDepth.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxDepth(string s) {
+    int maxCnt=0;
+    int open =0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='('){
+            open++;
+        }
+        else if (s[i]==')'){
+            maxCnt = max(maxCnt , open);
+            open--;              
+        }
+    }
+
+    return maxCnt;
+    
+}
