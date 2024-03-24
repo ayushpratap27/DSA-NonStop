493258d6a3b53f755bd32b5da00125fcbf78bce7
ayushpratap27 <ayushpratap2127@gmail.com>
2024-03-24 22:11:49 +0530

    Update findDuplicate.cpp

--- Array/Array Prac/findDuplicate.cpp
+++ Array/Array Prac/findDuplicate.cpp
@@ -11,3 +11,21 @@ vector<int> findDuplicates(vector<int>& nums) {
     }
     return result;
 }
+
+// int findDuplicate(vector<int>& nums) {
+//     int slow=nums[0];
+//     int fast=nums[0];
+
+//     do{
+//         slow= nums[slow];
+//         fast= nums[nums[fast]];
+//     } while(slow!=fast);
+
+//     slow=nums[0];
+//     while(slow!=fast) {
+//         slow=nums[slow];
+//         fast=nums[fast];
+//     }
+
+//     return slow;
+// }
