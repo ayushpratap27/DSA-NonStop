308f895752056826eece2c0683904373fb94a868
ayushpratap27 <ayushpratap2127@gmail.com>
2024-03-25 21:41:26 +0530

    Create scoreOfParenthesis.cpp

---
+++ Stack and Queues/scoreOfParentheses.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int ninjaParentheses(string S, int n)
+{
+	stack<int> m_stack;
+    m_stack.push(0); 
+    for(char c:S){
+        if(c=='(')
+            m_stack.push(0); 
+        else{
+            int tmp=m_stack.top(); 
+            m_stack.pop();
+            int val=0;
+            if(tmp>0) 
+                val=tmp*2;
+            else 
+                val=1;
+            m_stack.top()+=val; 
+        }   
+    }
+    return m_stack.top();
+}
