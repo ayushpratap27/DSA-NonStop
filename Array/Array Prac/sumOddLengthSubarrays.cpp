cfd083f4f2e54bec85be5d99c506677ba1bc87de
ayushpratap27 <ayushpratap2127@gmail.com>
2024-03-29 23:10:24 +0530

    Create sumOddLengthSubarrays.cpp

---
+++ Array/Array Prac/sumOddLengthSubarrays.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sumOddLengthSubarrays(vector<int>& arr){
+    int n=arr.size();
+    int sum=0;
+        
+    for(int i=0;i<n;i++)
+    {
+       sum = sum+(((n-i)*(i+1)+1)/2)*arr[i]; 
+    }
+    
+    return sum;
+    
+}
