18ab48b039cc68be398b675aaa4353c4ce8e5ccf
ayushpratap27 <ayushpratap2127@gmail.com>
2024-04-01 22:21:43 +0530

    Create noOfLeafNodes.cpp

---
+++ Binary Search Tree/noOfLeafNodes.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode {
+public :
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+    }
+};
+
+
+int noOfLeafNodes(BinaryTreeNode<int> *root) {
+    
+    if(root == NULL)
+        return 0;
+    
+    if(root->left == NULL && root->right == NULL)
+        return 1;
+    
+    int leftLeaves = noOfLeafNodes(root->left);
+    int rightLeaves = noOfLeafNodes(root->right);
+    
+    return leftLeaves + rightLeaves;
+}
