0d653377086fd6941cd09c4ba5e1543426495084
ayushpratap27 <ayushpratap2127@gmail.com>
2024-04-05 23:26:27 +0530

    Create makeGood.cpp

---
+++ Strings/makeGood.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string makeGood(string s) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string ans="";
+    stack<char> st;
+    for(int i=0;i<s.size();i++){
+        if(st.empty())
+            st.push(s[i]);
+        else if(abs(st.top()-s[i])==32)
+            st.pop();
+        else 
+            st.push(s[i]);
+    }
+
+    while(!st.empty()){
+        ans=st.top()+ans;
+        st.pop();
+    }
+        
+    return ans;
+}
