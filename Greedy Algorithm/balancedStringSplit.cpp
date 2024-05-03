1701239b969927f6e861ec097995e8621408d88d
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-03 22:02:21 +0530

    Create balancedStringSplit.cpp

---
+++ Greedy Algorithm/balancedStringSplit.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int balancedStringSplit(string s) {
+    int balance = 0;
+    int count = 0;
+    
+    for(char c : s) {
+        if(c == 'L')
+            balance++;
+        else
+            balance--;
+        
+        if(balance == 0)
+            count++;
+    }
+    
+    return count;
+}
