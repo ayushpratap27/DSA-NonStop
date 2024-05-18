d42ac82ef175ab018f92781d4959350602d74742
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-18 22:24:46 +0530

    Update twoSum.cpp

--- Array/Array Prac/twoSum.cpp
+++ Array/Array Prac/twoSum.cpp
@@ -1,24 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string read(int n, vector<int> book, int target)
-{
-    // Write your code here.
-    string s1="YES";
-    string s2="NO";
+//************ Brute Force******************
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    int n=nums.size();
+        
+    vector<int> result(2);
     for(int i=0;i<n;i++){
-        for(int j=1;j<n;j++){
-            if(book[i]+book[j]==target){
-                return s1;
+        for(int j=i;j<n;j++){
+            if(nums[i]+nums[j]==target && i!=j){
+                result[0]=i;
+                result[1]=j;
             }
         }
     }
 
-    return s2;
-}
+    return result;
 
-
-int main(){
-
-    
 }
