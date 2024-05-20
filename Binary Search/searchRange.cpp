0a43795580181418daa1b9df8067f3ea17ac0632
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-20 22:49:39 +0530

    Create searchRange.cpp

---
+++ Binary Search/searchRange.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> searchRange(vector<int>& nums, int target){
+    vector<int> v(2,-1);
+    int p=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]==target){
+            v[p++]=i;
+            break;
+        }
+    }
+
+    for(int j=nums.size()-1;j>=0;j--){
+        if(nums[j]==target){
+            v[p++]=j;
+            break;
+        }
+    }
+
+    return v;
+        
+}
