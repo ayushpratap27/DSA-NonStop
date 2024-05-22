b2e42ef0479518eb35ac38e3b3408db33579f67d
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-22 14:57:39 +0530

    Update moveZeroes.cpp

--- Array/Array Prac/moveZeroes.cpp
+++ Array/Array Prac/moveZeroes.cpp
@@ -17,8 +17,20 @@ void moveZeroes(vector<int>& nums) {
     }
         
 }
+//************** In place ****************
+void moveZeroes(vector<int>& nums) {
+    int k=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]!=0){
+            nums[k++]=nums[i];
+        }
+    }
+    for(int i=k;i<nums.size();i++){
+        nums[i]=0;
+    }
+}
 
-//************** Using Two Pointers***********
+//************** In place Using Two Pointers***********
 
 void moveZeroes(vector<int>& nums) {
     int i=0,j=0;
