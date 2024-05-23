b25f0d8b6fe49ca0f6837ef42df0ec2e76d6bbd2
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-23 18:33:00 +0530

    Create setZeroes.cpp

---
+++ Array/Array Prac/setZeroes.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//*************** Brute Force ****************
+void setZeroes(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(matrix[i][j] == 0){
+                for(int k=0; k<m; k++){
+                    if(matrix[i][k] != 0){
+                        matrix[i][k] = -9999;
+                    }
+                }
+            }
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(matrix[i][j] == 0){
+                for(int k=0; k<n; k++){
+                    if(matrix[k][j] != 0){
+                        matrix[k][j] = -9999;
+                    }
+                }
+            }
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(matrix[i][j] == -9999){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+// T.C.=O((n*m)*(n+m) + (n*m))
+// S.C.=O(1)
+
+
