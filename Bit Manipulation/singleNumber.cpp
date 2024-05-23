22abdd7b45422f0d5bbcd67ba64027c8a3f1547f
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-23 18:35:21 +0530

    Update singleNumber.cpp

--- Bit Manipulation/singleNumber.cpp
+++ Bit Manipulation/singleNumber.cpp
@@ -1,13 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int singleNumber(const std::vector<int>& nums) {
-    int result = 0;
+    int singleNumber(const std::vector<int>& nums) {
+        int result = 0;
+        
+        // XOR all elements in the array
+        for (int num : nums) {
+            result ^= num;
+        }
 
-    // XOR all elements in the array
-    for (int num : nums) {
-        result ^= num;
+        return result;
     }
+// T.C.=O(n)   S.C.=O(1)
 
-    return result;
+
+//************ Using Map to store Freq. ******************
+int singleNumber(vector<int>& nums) { 
+    unordered_map<int,int> a;
+    for(auto x: nums)
+	   a[x]++;
+    for(auto z:a)
+	    if(z.second==1)
+	 	   return z.first;
+	return -1;
 }
+
+// T.C.=O(n)  S.C.=O(n)
+
