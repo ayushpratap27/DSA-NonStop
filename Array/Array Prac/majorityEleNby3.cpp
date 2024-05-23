fbbf4c5eda885715e12ec85ccade85f7b1f3f8c5
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-24 00:25:59 +0530

    Update majorityEleNby3.cpp

--- Array/Array Prac/majorityEleNby3.cpp
+++ Array/Array Prac/majorityEleNby3.cpp
@@ -45,6 +45,46 @@ vector<int> majorityElement(vector<int>& nums) {
 
 }
 
-int main(){
-    
+vector<int> majorityElement(vector<int>& nums) {
+    int n=nums.size();
+    int cnt1=0, cnt2=0;
+    int ele1=INT_MIN, ele2=INT_MIN;
+
+    for(int i=0;i<n;i++){
+        if(cnt1==0 && ele2!=nums[i]){
+            ele1=nums[i];
+            cnt1=1;
+        }else if(cnt2==0 && ele1!=nums[i]){
+            ele2=nums[i];
+            cnt2=1;
+        }else if(nums[i]==ele1){
+            cnt1++;
+        }else if(nums[i]==ele2){
+            cnt2++;
+        }else{
+            cnt1--;
+            cnt2--;
+        }
+    }
+
+        int cnt3=0,cnt4=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele1){
+                cnt3++;
+            }
+            if(nums[i]==ele2){
+                cnt4++;
+            }
+        }
+        vector<int> result;
+        int mini = (int)(n/3)+1;
+        if(cnt3>=mini){
+            result.push_back(ele1);
+        }
+        if(cnt4>=mini){
+            result.push_back(ele2);
+        }
+        return result;
+
 }
+// T.C.=O(n) S.C.=O(1)
