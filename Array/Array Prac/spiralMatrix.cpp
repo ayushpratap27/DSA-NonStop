5e15e7d2a3144d18f15a273361427437787ec226
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-24 15:32:07 +0530

    Update spiralMatrix.cpp

--- Array/Array Prac/spiralMatrix.cpp
+++ Array/Array Prac/spiralMatrix.cpp
@@ -1,40 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void spiralMatrix(vector<vector<int>> &MATRIX){
-    // int n=MATRIX.size();
-    // int m=MATRIX[0].size();
-    int left=0,top=0;
-    int right=MATRIX[0].size()-1;
-    int bottom=MATRIX[0].size()-1;
-    int directon=0;
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    int n=matrix.size();
+    int m=matrix[0].size();
+    int left=0, right=m-1;
+    int top=0, bottom=n-1;
 
+    vector<int> ans;
     while(left<=right && top<=bottom){
-        if(direction==0){
-            for(int col=left;col<=right;col++){
-                cout<<MATRIX[top][col]<<" ";
-            }
-            top++;
-        }else if(direction==1){
-            for(int row=top;row<=bottom;row++){
-                cout<<MATRIX[row][right]<<" ";
-            }
-            right--;
-        }else if(direction==2){
-            for(int col=right;col>=left;col--){
-                cout<<MATRIX[bottom][col]<<" ";
+        for(int i=left;i<=right;i++){
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+        for(int i=top;i<=bottom;i++){
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+        if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                ans.push_back(matrix[bottom][i]);
             }
             bottom--;
-        }else{
-            for(int row=bottom;row>=top;row--){
-                cout<<MATRIX[row][left]<<" ";
+        }
+        if(left <= right){
+            for(int i=bottom;i>=top;i--){
+                ans.push_back(matrix[i][left]);
             }
             left++;
         }
-        direction=(direction+1)%4;
     }
+
+    return ans;
 }
 
-int main(){
-
-}
+//  T.C.=O(n*m)    S.C.=O(n*m)
