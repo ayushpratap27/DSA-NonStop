e4b76e761105ac58eaed00306eb0a0fed5791c8a
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-24 12:53:40 +0530

    Update 3Sum.cpp

--- Array/Array Prac/3Sum.cpp
+++ Array/Array Prac/3Sum.cpp
@@ -1,60 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> triplet(int n, vector<int> &arr) {
-    vector<vector<int>> result;
-    sort(arr.begin(), arr.end());
-
-    for(int i=0;i<n-2;i++){
-        
-        if(i>0 && arr[i]==arr[i-1]) continue;
-
-        int left = i+1;
-        int right = n-1;
-
-        while(left<right){
-            int current_sum = arr[i]+arr[left]+arr[right];
-
-            if(current_sum==0){
-                result.push_back({arr[i], arr[left], arr[right]});
-
-                // Skip duplicate elements
-                while(left<right && arr[left]==arr[left+1]) left++;
-                while(left<right && arr[right]==arr[right-1]) right--;
-
-                left++;
-                right--;
-            }else if(current_sum<0) {
-                left++;
-            }else{
-                right--;
+//*********** Brute Force ***************
+vector<vector<int>> threeSum(vector<int>& nums) {
+    set<vector<int>> st;
+    for(int i=0;i<nums.size();i++){
+        for(int j=i+1;j<nums.size();j++){
+            for(int k=j+1;k<nums.size();k++){
+                if(nums[i]+nums[j]+nums[k]==0){
+                    vector<int> temp={nums[i],nums[j],nums[k]};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
+                }
             }
         }
     }
-
+    vector<vector<int>> result(st.begin(),st.end());
     return result;
 }
+// T.C.=O(n^3 * log(no. of unique triplets))
+// S.C.=O(2*number of triplets)
 
-int main() {
-    int n;
-    cout << "Enter the size of the array: ";
-    cin >> n;
-
-    vector<int> arr(n);
-
-    cout << "Enter the elements of the array: ";
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-
-    vector<vector<int>> result = triplet(n, arr);
-
-    cout << "Triplets with sum zero are:" << endl;
-    for (const auto &triplet : result) {
-        cout << "[" << triplet[0] << ", " << triplet[1] << ", " << triplet[2] << "]" << endl;
-    }
-
-    return 0;
-}
