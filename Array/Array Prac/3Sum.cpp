d8561a299c9f64423f6a4cee0b4ec6de473d2727
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-24 12:54:08 +0530

    Update 3Sum.cpp

--- Array/Array Prac/3Sum.cpp
+++ Array/Array Prac/3Sum.cpp
@@ -21,3 +21,25 @@ vector<vector<int>> threeSum(vector<int>& nums) {
 // T.C.=O(n^3 * log(no. of unique triplets))
 // S.C.=O(2*number of triplets)
 
+//************* Better Approach ***************
+vector<vector<int>> threeSum(vector<int>& nums) {
+    int n=nums.size();
+    set<vector<int>> st;
+    for(int i=0;i<n;i++){
+        set<int> hashset;
+        for(int j=i+1;j<n;j++){
+            int third = -(nums[i]+nums[j]);
+            if(hashset.find(third)!=hashset.end()){
+                vector<int> temp={nums[i],nums[j],third};
+                sort(temp.begin(),temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(nums[j]);
+        }
+
+    }
+    vector<vector<int>> ans(st.begin(),st.end());
+    return ans;
+}
+// T.C.=O(n^2 * log(size of set(variable)))
+// S.C.=O(n) + O(2*no. of unique triplets)
