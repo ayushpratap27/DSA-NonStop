9d6d46fd82469a303a661e6b10ebd00ed2aded1d
ayushpratap27 <ayushpratap2127@gmail.com>
2024-05-31 14:27:49 +0530

    Update searchMatrix.cpp

--- Binary Search/searchMatrix.cpp
+++ Binary Search/searchMatrix.cpp
@@ -2,20 +2,25 @@
 using namespace std;
 
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    int n=matrix.size();
-    int m=matrix[0].size();
-    int i=0;
-    int j=m-1;
-    while(i<n && j>=0){
-        if(matrix[i][j]==target){
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int low = 0;
+    int high = n*m - 1;
+
+    while(low <= high){
+        int mid = (low + high)/2;
+        int row = mid/m, col = mid%m;
+
+        if(matrix[row][col] == target){
             return true;
-        }else if(matrix[i][j]<target){
-            i++;
+        }else if(matrix[row][col] < target){
+            low = mid+1;
         }else{
-            j--;
+            high = mid-1;
         }
     }
 
     return false;
-
 }
+//  T.C.= O(log(n*m)), where n is the number of rows in the matrix and m is the number of columns
+//  S.C.= O(1)
