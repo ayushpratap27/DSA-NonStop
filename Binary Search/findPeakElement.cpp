d629e1c45ca5ac6c0d3717d5b610e2df25360cbc
ayushpratap27 <ayushpratap2127@gmail.com>
2024-06-01 06:10:10 +0530

    Update findPeakElement.cpp

--- Binary Search/findPeakElement.cpp
+++ Binary Search/findPeakElement.cpp
@@ -1,19 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int findPeakElement(const vector<int>& nums) {
-    int left = 0;
-    int right = nums.size() - 1;
+int findPeakElement(vector<int>& nums) {
+    int n = nums.size();
+    if(n == 1) return 0;
+    if(nums[0] > nums[1]) return 0;
+    if(nums[n-1] > nums[n-2]) return n-1;
 
-    while (left < right) {
-        int mid = left + (right - left) / 2;
-
-        if (nums[mid] > nums[mid + 1]) {
-            right = mid;
-        } else {
-            left = mid + 1;
+    int low = 1, high = n-2;
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+            return mid;
+        }else if(nums[mid] > nums[mid-1]){
+            low = mid+1;
+        }else{
+            high = mid-1;
         }
     }
 
-    return left;
+    return -1;
 }
+//  T.C.= O(logn)  S.C.= O(1)
