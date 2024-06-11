98a751a182bcf848b16451d150e11b49446317fa
ayushpratap27 <ayushpratap2127@gmail.com>
2024-06-11 22:30:40 +0530

    Update singleNumber.cpp

--- Bit Manipulation/singleNumber.cpp
+++ Bit Manipulation/singleNumber.cpp
@@ -1,16 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-    int singleNumber(const std::vector<int>& nums) {
-        int result = 0;
+int singleNumber(const std::vector<int>& nums) {
+    int result = 0;
         
-        // XOR all elements in the array
-        for (int num : nums) {
-            result ^= num;
-        }
-
-        return result;
+    // XOR all elements in the array
+    for (int num : nums) {
+        result ^= num;
     }
+
+    return result;
+}
 // T.C.=O(n)   S.C.=O(1)
 
 
