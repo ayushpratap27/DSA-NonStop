e670a80d3a34e11dd0a81e059b0adfd6b9adab31
ayushpratap27 <ayushpratap2127@gmail.com>
2024-06-13 23:03:35 +0530

    Update bfsTraversal.cpp

--- Graphs/bfsTraversal.cpp
+++ Graphs/bfsTraversal.cpp
@@ -24,3 +24,13 @@ vector<int> bfsTraversal(int n, vector<vector<int>> &adj){
 
     return bfs;
 }
+
+/*
+Time complexity: O(V + E), where V is the number of vertices and E is the number of edges in the graph.
+This is because in the worst case scenario, we visit each vertex and each edge once during the BFS 
+traversal.
+
+Space complexity: O(V), where V is the number of vertices in the graph. This is because we are using 
+a queue to store the vertices to be visited, and in the worst case scenario, the queue can contain all 
+the vertices in the graph.
+*/
