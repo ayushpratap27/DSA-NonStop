3cdc7d651f25202ad5f3002c837e490b9470c18f
ayushpratap27 <ayushpratap2127@gmail.com>
2024-07-26 22:30:38 +0530

    Update frogJump.cpp

--- DP/DP on strings/frogJump.cpp
+++ DP/DP on strings/frogJump.cpp
@@ -21,3 +21,41 @@ int frogJump(int n, vector<int> &heights)
 
 // T.C. = O(n)
 // S.C. = O(n) + o(n)
+
+// using tabulation method
+
+int frogJump(int n, vector<int> &heights){
+    vector<int> dp(n,0);
+    dp[0] = 0;
+    for(int i=1;i<n;i++){
+        int fs = dp[i-1] + abs(heights[i] - heights[i-1]);
+        int ss = INT_MAX;
+        if(i > 1){
+            ss = dp[i-2] + abs(heights[i] - heights[i-2]);
+        }
+
+        dp[i] = min(fs, ss);
+    }
+
+    return dp[n-1];
+}
+
+// space optimised approach
+
+int frogJump(int n, vector<int> &heights){
+    int prev = 0;
+    int prev2 = 0;
+    for(int i=1;i<n;i++){
+        int fs = prev + abs(heights[i] - heights[i-1]);
+        int ss = INT_MAX;
+        if(i > 1){
+            ss = prev2 + abs(heights[i] - heights[i-2]);
+        }
+
+        int curri = min(fs, ss);
+        prev2 = prev;
+        prev = curri;
+    }
+
+    return prev;
+}
