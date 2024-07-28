dbfd6179c9193f83d9d38639d339ed04277ef513
ayushpratap27 <ayushpratap2127@gmail.com>
2024-07-28 22:44:12 +0530

    Update smallestDivisor.cpp

--- Binary Search/smallestDivisor.cpp
+++ Binary Search/smallestDivisor.cpp
@@ -2,23 +2,27 @@
 using namespace std;
 
 int smallestDivisor(vector<int>& nums, int threshold) {
-    int left = 1;
-    int right = *max_element(nums.begin(), nums.end());
+    int low = 1;
+    int high = *max_element(nums.begin(), nums.end());
 
-    while (left < right) {
-        int mid = left + (right - left) / 2;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
         int sum = 0;
 
-        for (int num : nums) {
-            sum +=ceil(static_cast<double>(num) / mid);
+        for(int num : nums){
+            sum += ceil((double)(num) / (double)(mid));
         }
 
-        if (sum > threshold) {
-            left = mid + 1;
+        if(sum <= threshold) {
+            high = mid-1;
         } else {
-            right = mid;
+            low = mid+1;
         }
     }
 
-    return left;
+    return low;
 }
+
+//  T.C.= O(n * log(max(nums))), where n is the number of elements in the 
+//  input vector nums and max(nums) is the maximum element in nums
+//  S.C.= O(1)
