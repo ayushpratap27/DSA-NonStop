894d79a27832037a4aadf041cba0a7f98f18fbd6
ayushpratap27 <ayushpratap2127@gmail.com>
2024-08-26 23:32:34 +0530

    Update minCostClimbingStairs.cpp

--- DP/DP on strings/minCostClimbingStairs.cpp
+++ DP/DP on strings/minCostClimbingStairs.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+//           Memoization
 int f(int i, vector<int> &cost, vector<int> &dp){
     if(i<=1) return 0;
     if(dp[i] != -1) return dp[i];
@@ -14,5 +16,21 @@ int minCostClimbingStairs(vector<int>& cost) {
     vector<int>dp(n+1, -1);
 
     return f(n, cost, dp);
+        
+}
+
+//          Tabulation
+
+int minCostClimbingStairs(vector<int>& cost) {
+    int n = cost.size();
+    vector<int>dp(n+1, 0);
+        
+    for(int i=2; i<=n; i++){
+        int mini = min(cost[i-1] + dp[i-1], cost[i-2] + dp[i-2]);
+
+        dp[i] = mini;
+    }
+
+    return dp[n];
         
 }
