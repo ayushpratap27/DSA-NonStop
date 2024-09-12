f6b9e9dfbba81c9b3631dcf33a5f27b6bd187155
ayushpratap27 <ayushpratap2127@gmail.com>
2024-09-12 14:49:45 +0530

    Create lowestCommonAncestor.cpp

---
+++ Binary Tree/lowestCommonAncestor.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+    if (root == NULL || root == p || root == q)
+    {
+        return root;
+    }
+
+    TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+    if (left == NULL)
+    {
+        return right;
+    }
+    else if (right == NULL)
+    {
+        return left;
+    }
+    else // both left and right are not NULL, we found our result
+    {
+        return root;
+    }
+}
+
+//  T.C. = O(N)
+//  S.C. = O(N)
