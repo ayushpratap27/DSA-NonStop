e72f01332e4379552f2f3cbe125bba9de505123f
ayushpratap27 <ayushpratap2127@gmail.com>
2024-09-14 21:53:41 +0530

    Create stableMountains.cpp

---
+++ Array/Array Prac/stableMountains.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> stableMountains(vector<int> &height, int threshold)
+{
+    vector<int> ans;
+    int n = height.size();
+    for (int i = 1; i < n; i++)
+    {
+        if (height[i - 1] > threshold)
+        {
+            ans.push_back(i);
+        }
+    }
+
+    return ans;
+}
