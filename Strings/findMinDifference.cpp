2a3b1c923902a087df5ad4397c216444af55d61a
ayushpratap27 <ayushpratap2127@gmail.com>
2024-09-16 22:50:56 +0530

    Update findMinDifference.cpp

--- Strings/findMinDifference.cpp
+++ Strings/findMinDifference.cpp
@@ -13,16 +13,14 @@ int findMinDifference(vector<string> &timePoints)
     }
 
     int ans = INT_MAX;
-    for (int i = 0; i < n; i++)
-    {
-        v.push_back(abs(v[i] + 24 * 60));
-    }
     sort(v.begin(), v.end());
 
-    for (int i = 0; i < 2 * n - 1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         ans = min(ans, v[i + 1] - v[i]);
     }
+
+    ans = min(ans, abs(v[n - 1] - v[0] - 24 * 60));
 
     return ans;
 }
