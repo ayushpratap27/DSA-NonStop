4e431a389e18aa4cf5bc019dbb05196dcdf259f9
ayushpratap27 <ayushpratap2127@gmail.com>
2024-09-16 22:34:27 +0530

    Create findMinDifference.cpp

---
+++ Strings/findMinDifference.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMinDifference(vector<string> &timePoints)
+{
+    int n = timePoints.size();
+    vector<int> v(n, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        v[i] = (int(timePoints[i][0]) - 48) * 600 + (int(timePoints[i][1]) - 48) * 60 +
+               (int(timePoints[i][3]) - 48) * 10 + (int(timePoints[i][4]) - 48);
+    }
+
+    int ans = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        v.push_back(abs(v[i] + 24 * 60));
+    }
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        ans = min(ans, v[i + 1] - v[i]);
+    }
+
+    return ans;
+}
