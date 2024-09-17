b5bd85c87f69cbf47f4eed02f503b3e2363d44ee
ayushpratap27 <ayushpratap2127@gmail.com>
2024-09-17 12:18:58 +0530

    Update insertionInBST.cpp

--- Binary Search Tree/insertionInBST.cpp
+++ Binary Search Tree/insertionInBST.cpp
@@ -1,8 +1,8 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-struct TreeNode {
+struct TreeNode
+{
     int val;
     TreeNode *left;
     TreeNode *right;
@@ -11,17 +11,33 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-TreeNode* insertIntoBST(TreeNode* root, int val) {
+TreeNode *insertIntoBST(TreeNode *root, int val){
 
-    if(root==NULL){
-        return new TreeNode(val);
+    TreeNode *curr = root;
+    TreeNode *newNode = new TreeNode(val);
+    if (root == NULL)
+        return newNode;
+    while (true){
+        if (curr->val >= val)
+        {
+            if (curr->left != NULL)
+                curr = curr->left;
+            else
+            {
+                curr->left = newNode;
+                break;
+            }
+        }
+        else
+        {
+            if (curr->right != NULL)
+                curr = curr->right;
+            else
+            {
+                curr->right = newNode;
+                break;
+            }
+        }
     }
-    if(val<root->val){
-        root->left=insertIntoBST(root->left, val);
-    }
-    else{
-        root->right=insertIntoBST(root->right, val);
-    }
-
     return root;
 }
