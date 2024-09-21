c8697b38a4c02b79fc94821d8e26c244aa1c7d88
ayushpratap27 <ayushpratap2127@gmail.com>
2024-09-21 16:05:55 +0530

    Create lexicalOrder.cpp

---
+++ Array/Array Prac/lexicalOrder.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> lexicalOrder(int n){
+    vector<int> v;
+    int curr = 1;
+    for (int i = 0; i < n; i++){
+        v.push_back(curr);
+        if (curr * 10 <= n){
+            curr *= 10;
+        }
+        else{
+            while (curr % 10 == 9 || curr + 1 > n){
+                curr /= 10;
+            }
+            curr++;
+        }
+    }
+    return v;
+}
