708a0839a40d4f120b4e6b4461680ad7e2e9c5a2
ayushpratap27 <ayushpratap2127@gmail.com>
2024-09-28 21:53:57 +0530

    Create numSubarraysWithSum.cpp

---
+++ Two Pointers/numSubarraysWithSum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int count(vector<int> &nums, int goal){
+    if (goal < 0)
+        return 0;
+    int n = nums.size();
+    int l = 0, r = 0, sum = 0, cnt = 0;
+    while (r < n){
+        sum += nums[r];
+        while (sum > goal){
+            sum -= nums[l];
+            l++;
+        }
+        cnt += (r - l + 1);
+        r++;
+    }
+    return cnt;
+}
+
+int numSubarraysWithSum(vector<int> &nums, int goal){
+    int x = count(nums, goal);
+    int y = count(nums, goal - 1);
+
+    return x - y;
+}
