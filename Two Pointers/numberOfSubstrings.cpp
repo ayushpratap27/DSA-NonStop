1c1e1a80f3a118aaef60ca0e030940db6ad23e63
ayushpratap27 <ayushpratap2127@gmail.com>
2024-09-29 21:48:10 +0530

    Update numberOfSubstrings.cpp

--- Two Pointers/numberOfSubstrings.cpp
+++ Two Pointers/numberOfSubstrings.cpp
@@ -1,4 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+bool allLetters(unordered_map<char, int> &mp){
+    return mp['a'] > 0 && mp['b'] > 0 && mp['c'] > 0;
+}
 
+int numberOfSubstrings(string s){
+    int n = s.size();
+    int cnt = 0, l = 0, r = 0;
+    unordered_map<char, int> mp;
+    while (r < n){
+        mp[s[r]]++;
+        while (allLetters(mp)){
+            cnt += (n - r);
+            mp[s[l]]--;
+            l++;
+        }
+        r++;
+    }
+    return cnt;
+}
