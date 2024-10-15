49f3d2412519b906a4397a5efccedeeaf86e0cfb
Ayush Pratap <ayushpratap2127@gmail.com>
2024-10-15 10:21:49 +0530

    Update reverseStringWordWise.cpp

--- Strings/reverseStringWordWise.cpp
+++ Strings/reverseStringWordWise.cpp
@@ -1,31 +1,31 @@
-#include <bits/stdc++.h>>
+#include<bits/stdc++.h>
 using namespace std;
 
+void reverseStringWordWise(char input[]) {
+    int length = strlen(input);
+    int start = 0;
 
-string reverseStringWordWise(string input){
-    vector<string> words;
-    stringstream ss(input);
-    string word;
-
-    while(ss>>word){
-        words.push_back(word);
+    for (int i=0;i<length/2;i++){
+        char temp = input[i];
+        input[i] = input[length-i- 1];
+        input[length-i-1] = temp;
     }
 
-    string reversedString;
-
-    for(int i=words.size()-1;i>=0;i--){
-        reversedString += words[i];
-        if(i!=0) {
-            reversedString += " ";
+    for(int i=0;i<length;i++){
+        if(input[i]==' '){
+            for(int j=start;j<(i+start)/2;j++){
+                char temp=input[j];
+                input[j]=input[i-j+start-1];
+                input[i-j+start-1]=temp;
+            }
+        
+            start=i+1;
         }
     }
 
-    return reversedString;
-}
-
-int main(){
-    string s;
-    getline(cin, s);
-    string ans = reverseStringWordWise(s);
-    cout << ans << endl;
+    for(int j=start;j<(length+start)/2;j++){
+        char temp = input[j];
+        input[j] = input[length-j+start-1];
+        input[length-j+start-1] = temp;
+    }
 }
