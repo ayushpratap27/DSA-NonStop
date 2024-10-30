bec2a78fb82dbfa578c84f8fc06ec58a903f9424
Ayush Pratap <ayushpratap2127@gmail.com>
2024-10-30 19:42:15 +0530

    Update climbStairs.cpp

--- DP/climbStairs.cpp
+++ DP/climbStairs.cpp
@@ -21,3 +21,16 @@ int climbStairs(int n){
 
     return f(n, dp);
 }
+
+//   Tabulation approach
+
+int climbStairs(int n) {
+        vector<int> dp(n+1, 0);
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+}
