bdf736708f7ecec40db7cfd5c07837360b8bf274
Ayush Pratap <ayushpratap2127@gmail.com>
2024-11-13 22:17:06 +0530

    Create countFairPairs.cpp

---
+++ Two Pointers/countFairPairs.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+long long countPairs(vector<int> &nums, int target){
+    long long count = 0;
+    int left = 0, right = nums.size() - 1;
+
+    while (left < right){
+        if (nums[left] + nums[right] > target)
+            right--;
+        else{
+            count += right - left;
+            left++;
+        }
+    }
+    return count;
+}
+
+long long countFairPairs(vector<int> &nums, int lower, int upper){
+    sort(nums.begin(), nums.end());
+    return countPairs(nums, upper) - countPairs(nums, lower - 1);
+}
