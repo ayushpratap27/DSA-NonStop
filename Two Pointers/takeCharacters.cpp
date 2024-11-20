6f56593b355f6cab0a131d7c3dff5b96ac5610d0
Ayush Pratap <ayushpratap2127@gmail.com>
2024-11-20 21:45:56 +0530

    Create takeCharacters.cpp

---
+++ Two Pointers/takeCharacters.cpp
@@ -0,0 +1,101 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int takeCharacters(string s, int k){
+    vector<int> count(3, 0);
+    for (char c : s){
+        count[c - 'a']++;
+    }
+
+    if (*min_element(count.begin(), count.end()) < k){
+        return -1;
+    }
+
+    int res = INT_MAX;
+    int l = 0;
+    for (int r = 0; r < s.length(); r++){
+        count[s[r] - 'a']--;
+
+        while (*min_element(count.begin(), count.end()) < k){
+            count[s[l] - 'a']++;
+            l++;
+        }
+        res = min(res, static_cast<int>(s.length()) - (r - l + 1));
+    }
+    return res;
+}
