7b688af8fcee680ff434a910be043a2b1e04ea3a
Ayush Pratap <ayushpratap2127@gmail.com>
2024-11-22 22:43:35 +0530

    Create maxEqualRowsAfterFlip.cpp

---
+++ Array/maxEqualRowsAfterFlip.cpp
@@ -0,0 +1,99 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+    unordered_map<string, int> relative_freq;
+    for (auto &row : matrix){
+        int first = row[0];
+        string curr;
+
+        for (int ele : row){
+            if (ele == first)
+                curr.push_back('0');
+            else
+                curr.push_back('1');
+        }
+        relative_freq[curr]++;
+    }
+    int max_freq = 0;
+    for (auto &[string, freq] : relative_freq){
+        max_freq = max(max_freq, freq);
+    }
+
+    return max_freq;
+}
