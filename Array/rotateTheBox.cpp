ed75eb6fbe229f92ace1c0c633109d964e18de0d
Ayush Pratap <ayushpratap2127@gmail.com>
2024-11-23 23:26:58 +0530

    Create rotateTheBox.cpp

---
+++ Array/rotateTheBox.cpp
@@ -0,0 +1,100 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+vector<vector<char>> rotateTheBox(vector<vector<char>> &box){
+    int ROWS = box.size();
+    int COLS = box[0].size();
+
+    vector<vector<char>> res(COLS, vector<char>(ROWS, '.'));
+
+    for (int r = 0; r < ROWS; r++){
+        int i = COLS - 1;
+        for (int c = COLS - 1; c >= 0; c--){
+            if (box[r][c] == '#'){
+                res[i][ROWS - r - 1] = '#';
+                i--;
+            }
+            else if (box[r][c] == '*'){
+                res[c][ROWS - r - 1] = '*';
+                i = c - 1;
+            }
+        }
+    }
+
+    return res;
+}
