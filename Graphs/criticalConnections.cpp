2720f70ef234e0c0a35ec30d2fbe6b910d415ae9
Ayush Pratap <ayushpratap2127@gmail.com>
2024-12-05 22:14:55 +0530

    Create criticalConnections.cpp

---
+++ Graphs/criticalConnections.cpp
@@ -0,0 +1,116 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+class Solution {
+    private:
+    int timer = 1;
+    private:
+    void dfs(int node, int parent, vector<int> &vis, vector<int> adj[], int tin[], int low[], vector<vector<int>> &bridges){
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        for(auto it : adj[node]){
+            if(it == parent) continue;
+            if(vis[it] == 0){
+                dfs(it, node, vis, adj, tin, low, bridges);
+                low[node] = min(low[node], low[it]);
+                if(low[it] > tin[node]){
+                    bridges.push_back({it, node});
+                }
+            }
+            else {
+                low[node] = min(low[node], low[it]);
+            }
+        }
+    }
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<int> adj[n];
+        for(auto it : connections){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> vis(n, 0);
+        int tin[n];
+        int low[n];
+        vector<vector<int>> bridges;
+        dfs(0, -1, vis, adj, tin, low, bridges);
+
+        return bridges;
+    }
+};
