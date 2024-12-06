942e77031c4a652e9cb6bba5d0dbad962b45c078
Ayush Pratap <ayushpratap2127@gmail.com>
2024-12-06 21:48:21 +0530

    Create findDisappearedNumber.cpp

---
+++ Array/Array Prac/findDisappearedNumber.cpp
@@ -0,0 +1,96 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+    int n = nums.size();
+    for (int i = 0; i < n; i++){
+        while (nums[i] != (i + 1) && nums[i] != nums[nums[i] - 1]){
+            int temp = nums[i];
+            nums[i] = nums[nums[i] - 1];
+            nums[temp - 1] = temp;
+        }
+    }
+
+    vector<int> result;
+    for (int i = 0; i < n; i++){
+        if (nums[i] != i + 1){
+            result.push_back(i + 1);
+        }
+    }
+    return result;
+}
