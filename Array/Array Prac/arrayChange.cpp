a58fdfecfc8b6160477f1c8292545af1210d5d82
Ayush Pratap <ayushpratap2127@gmail.com>
2024-12-07 16:35:54 +0530

    Create arrayChange.cpp

---
+++ Array/Array Prac/arrayChange.cpp
@@ -0,0 +1,99 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+    int n = nums.size();
+    int m = operations.size();
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++){
+        mp[nums[i]] = i;
+    }
+
+    for (auto it : operations){
+        int first = it[0];
+        int second = it[1];
+
+        int idx = mp[first];
+        nums[idx] = second;
+
+        mp.erase(first);
+        mp[second] = idx;
+    }
+
+    return nums;
+}
