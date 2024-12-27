2e9a3c6ea3b996534c05a8cee1a6efaae22ce68c
Ayush Pratap <ayushpratap2127@gmail.com>
2024-12-27 15:51:28 +0530

    Create intervalIntersection.cpp

---
+++ Two Pointers/intervalIntersection.cpp
@@ -0,0 +1,100 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+    int n = firstList.size();
+    int m = secondList.size();
+    vector<vector<int>> ans;
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        int startPoint = max(firstList[i][0], secondList[j][0]);
+        int endPoint = min(firstList[i][1], secondList[j][1]);
+        if (startPoint <= endPoint) {
+            ans.push_back({startPoint, endPoint});
+        }
+
+        if (firstList[i][1] <= secondList[j][1]) {
+            i++;
+        }
+        else {
+            j++;
+        }
+    }
+
+    return ans;
+}
