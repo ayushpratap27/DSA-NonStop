880cf432342ee99a6575d7c05ad71cb26b4ef9f4
Ayush Pratap <ayushpratap2127@gmail.com>
2024-12-29 22:07:14 +0530

    Create compareVersion.cpp

---
+++ Two Pointers/compareVersion.cpp
@@ -0,0 +1,104 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int compareVersion(string version1, string version2) {
+    int i = 0;
+    int j = 0;
+    while (i < version1.size() || j < version2.size()) {
+        int num1 = 0;
+        int num2 = 0;
+
+        while (i < version1.size() && version1[i] != '.') {
+            num1 = num1 * 10 + (version1[i] - '0');
+            i++;
+        }
+
+        while (j < version2.size() && version2[j] != '.') {
+            num2 = num2 * 10 + (version2[j] - '0');
+            j++;
+        }
+
+        if (num1 < num2) return -1;
+        if (num1 > num2) return 1;
+
+        i++;
+        j++;
+    }
+
+    return 0;
+}
