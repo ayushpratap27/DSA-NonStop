74d222d428a663701d861a957c4c9ba3cf7f1984
Ayush Pratap <ayushpratap2127@gmail.com>
2025-01-07 21:34:59 +0530

    Create candy.cpp

---
+++ Array/Array Prac/candy.cpp
@@ -0,0 +1,141 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int candy(vector<int>& ratings) {
+    int n = ratings.size();
+    vector<int> left(n);
+    left[0] = 1;
+
+    for(int i = 1; i < n; i++){
+        if(ratings[i] > ratings[i-1]){
+            left[i] = left[i-1] + 1;
+        }else {
+            left[i] = 1;
+        }
+    }
+    int curr = 1, right = 1;
+    int sum = max(1, left[n-1]);
+    for(int i = n - 2; i >= 0; i--){
+        if(ratings[i] > ratings[i+1]){
+            curr = right + 1;
+        }else {
+            curr = 1;
+        }
+        right = curr;
+        sum += max(left[i], curr);
+    }
+
+    return sum;
+}
+// T.C. - O(2N)
+// S.C. - O(N)
+
+int candy(vector<int> &ratings){
+    int n = ratings.size();
+    vector<int> left(n);
+    vector<int> right(n);
+    left[0] = 1;
+    right[n - 1] = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            left[i] = left[i - 1] + 1;
+        }
+        else {
+            left[i] = 1;
+        }
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            right[i] = right[i + 1] + 1;
+        }
+        else {
+            right[i] = 1;
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += max(left[i], right[i]);
+    }
+
+    return sum;
+}
+// T.C. - O(3N)
+// S.C. - O(2N)
