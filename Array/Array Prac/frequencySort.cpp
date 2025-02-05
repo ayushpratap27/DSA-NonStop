4573ce86dfcfe3b64cb68d6ab82b7022f01267ff
Ayush Pratap <ayushpratap2127@gmail.com>
2025-02-05 23:29:08 +0530

    Create frequencySort.cpp

---
+++ Array/Array Prac/frequencySort.cpp
@@ -0,0 +1,107 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+static bool compare(const pair<int, int>& a, const pair<int, int>& b) {
+    if (a.first == b.first)
+        return a.second > b.second;
+    return a.first < b.first;
+}
+
+vector<int> frequencySort(vector<int> &nums){
+    unordered_map<int, int> mp;
+
+    for (int num : nums) {
+        mp[num]++;
+    }
+
+    vector<pair<int, int>> freqPairs;
+    for (auto it : mp) {
+        freqPairs.push_back({it.second, it.first});
+    }
+
+    sort(freqPairs.begin(), freqPairs.end(), compare);
+
+    vector<int> ans;
+    for (auto it : freqPairs) {
+        while (it.first--){
+            ans.push_back(it.second);
+        }
+    }
+
+    return ans;
+}
