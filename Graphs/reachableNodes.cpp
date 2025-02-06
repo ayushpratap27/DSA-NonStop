1bd80851cf83b86edfd9a6e871ce97950875d8db
Ayush Pratap <ayushpratap2127@gmail.com>
2025-02-06 22:28:57 +0530

    Create reachableNodes.cpp

---
+++ Graphs/reachableNodes.cpp
@@ -0,0 +1,106 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+void dfs( int node, vector<int> adj[], vector<int> &vis, int &cnt){
+    vis[node] = 1;
+    cnt++;
+
+    for (auto it : adj[node]) {
+        if (!vis[it]) dfs(it, adj, vis, cnt);
+    }
+
+}
+
+int reachableNodes(int n, vector<vector<int>> &edges, vector<int> &restricted){
+    vector<int> adj[n];
+    for(int i = 0; i < edges.size(); i++){
+        adj[edges[i][0]].push_back(edges[i][1]);
+        adj[edges[i][1]].push_back(edges[i][0]);
+    }
+
+    vector<int> vis(n, 0);
+    int cnt = 0;
+
+    for(int i = 0; i < restricted.size(); i++){
+        vis[restricted[i]] = 1;
+    }
+
+    dfs(0, adj, vis, cnt);
+
+    return cnt;
+}
