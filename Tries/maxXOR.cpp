0e0e6ac47cabf257b70007fb4133ef91f05cc27e
Ayush Pratap <ayushpratap2127@gmail.com>
2025-02-15 17:34:50 +0530

    Create maxXOR.cpp

---
+++ Tries/maxXOR.cpp
@@ -0,0 +1,142 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+struct Node {
+    Node* links[2];
+
+    bool containsKey(int bit) {
+        return (links[bit] != NULL);
+    }
+
+    Node* get(int bit) {
+        return links[bit];
+    }
+
+    void put(int bit, Node* node) {
+        links[bit] = node;
+    }
+};
+
+class Trie {
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(int num) {
+        Node* node = root;
+        for(int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            if(!node->containsKey(bit)){
+                node->put(bit, new Node());
+            }
+            node = node->get(bit);
+        }
+    }
+
+    int getMax(int num) {
+        Node* node = root;
+        int maxNum = 0;
+        for(int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            if(node->containsKey(1 - bit)) {
+                maxNum = maxNum | (1<<i);
+                node = node->get(1 - bit);
+            } else {
+                node = node->get(bit);
+            }
+        }
+
+        return maxNum;
+    }
+};
+
+int maxXOR(int n, int m, vector<int> &arr1, vector<int> &arr2) {
+    
+    Trie trie;
+    for(auto &it : arr1) trie.insert(it);
+    int maxi = 0;
+    for(auto &it : arr2) {
+        maxi = max(maxi, trie.getMax(it));
+    }
+
+    return maxi;
+}
+
+// T.C. -> O(N x 32) + O(M x 32)
