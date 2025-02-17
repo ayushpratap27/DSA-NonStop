703b827a96658c6b89fd948500065d3b13348598
Ayush Pratap <ayushpratap2127@gmail.com>
2025-02-17 22:33:56 +0530

    Create numTilePossibilities.cpp

---
+++ Recursion/numTilePossibilities.cpp
@@ -0,0 +1,103 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+void f(string &tiles, vector<bool> &vis, string str, unordered_set<string> &st){
+    if(str.length() != 0){
+        st.insert(str);
+    }
+
+    for(int i = 0; i < tiles.size(); i++) {
+        if(!vis[i]){
+            vis[i] = true;
+            f(tiles, vis, str + tiles[i], st);
+            vis[i] = false;
+        }
+    }
+}
+
+int numTilePossibilities(string tiles) {
+    int n = tiles.size();
+    unordered_set<string> st;
+    vector<bool> vis(n, false);
+    f(tiles, vis, "", st);
+
+    reverse(tiles.begin(), tiles.end());
+    f(tiles, vis, "", st);
+
+    return st.size();
+}
