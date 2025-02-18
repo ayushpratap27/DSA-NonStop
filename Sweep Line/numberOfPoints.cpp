f10ea853a5a3998d7c378121160d8a087b1a6a1a
Ayush Pratap <ayushpratap2127@gmail.com>
2025-02-18 23:11:54 +0530

    Create numberOfPoints.cpp

---
+++ Sweep Line/numberOfPoints.cpp
@@ -0,0 +1,104 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int numberOfPoints(vector<vector<int>>& nums) {
+    // vector<int> line(102, 0);
+    // for(auto& p : nums){
+    //     line[p[0]]++;
+    //     line[p[1] + 1]--;
+    // }
+    // int ans = 0;
+    // int count = 0;
+    // for(int i = 0; i < 102; i++){
+    //     count += line[i];
+    //     if(count > 0){
+    //         ans++;
+    //     }
+    // }
+    // return ans;
+    
+    set<int> points;
+
+    for(auto it : nums){
+        for(int i = it[0]; i <= it[1]; i++){
+            points.insert(i);
+        }
+    }
+
+    return points.size();
+}
