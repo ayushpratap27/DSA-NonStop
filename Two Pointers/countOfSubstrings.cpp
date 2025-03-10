46d7114eeaf360f8cdb29d19378345d3ece24a9c
Ayush Pratap <ayushpratap2127@gmail.com>
2025-03-10 22:12:08 +0530

    Create countOfSubstrings.cpp

---
+++ Two Pointers/countOfSubstrings.cpp
@@ -0,0 +1,102 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+bool isVowel(char ch) {
+    return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+}
+
+long long atMostK(string& word, int k) {
+    int n = word.size(), l = 0, cons = 0;
+    long long cnt = 0;
+    unordered_map<char, int> mp;
+    
+    for (int r = 0; r < n; r++) {
+        isVowel(word[r]) ? mp[word[r]]++ : cons++;
+        
+        while (mp.size() == 5 && cons > k) {
+            isVowel(word[l]) ? (mp[word[l]]--, mp[word[l]] == 0 ? mp.erase(word[l]) : 0) : cons--;
+            l++;
+        }
+        cnt += (r - l + 1);
+    }
+    return cnt;
+}
+
+long long countOfSubstrings(string word, int k) {
+    return atMostK(word, k) - atMostK(word, k - 1);
+}
