870772c6432448ec84e1c9c22d0476203dce26b9
Ayush Pratap <ayushpratap2127@gmail.com>
2025-03-15 22:28:11 +0530

    Create totalNumbers.cpp

---
+++ Basics/totalNumbers.cpp
@@ -0,0 +1,98 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int totalNumbers(vector<int>& digits) {
+    set<int> st;
+    int n = digits.size();
+    
+    for(int i = 0; i < n; i++) {     
+        if(digits[i] == 0) continue;    
+        
+        for(int j = 0; j < n; j++) {      
+            if(j == i) continue;          
+            for(int k = 0; k < n; k++) { 
+                if(k == i || k == j) continue;  
+                if(digits[k] % 2 == 0){ 
+                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                    st.insert(num);
+                }
+            }
+        }
+    }
+    return st.size();
+}
