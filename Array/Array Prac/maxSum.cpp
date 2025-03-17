91dc1c9659cef9afdfdf260752e5d53b4cea6995
Ayush Pratap <ayushpratap2127@gmail.com>
2025-03-17 22:12:12 +0530

    Create maxSum.cpp

---
+++ Array/Array Prac/maxSum.cpp
@@ -0,0 +1,100 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int maxSum(vector<int>& nums) {
+    unordered_set<int> st;
+    int maxi = INT_MIN;
+    bool hasPositive = false, hasZero = false;
+    
+    for(int num : nums){
+        if(num > 0){
+            st.insert(num);
+            hasPositive = true;
+        }else if(num == 0){
+            hasZero = true;
+        }else{
+            maxi = max(maxi, num);
+        }
+    }
+    
+    if(hasPositive){
+        return accumulate(st.begin(), st.end(), 0);
+    }
+    
+    return hasZero ? 0 : maxi;
+}
