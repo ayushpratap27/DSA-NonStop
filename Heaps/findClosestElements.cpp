4f9129838739cedc1fc8734719cf2020b2d3fbe0
Ayush Pratap <ayushpratap2127@gmail.com>
2025-03-20 22:02:53 +0530

    Create findClosestElements.cpp

---
+++ Heaps/findClosestElements.cpp
@@ -0,0 +1,98 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    int n = arr.size();
+
+    priority_queue<pair<int, int>> pq;
+
+    for(auto ele : arr) {
+        pq.push({abs(x - ele), ele});
+        if(pq.size() > k) pq.pop();
+    }
+
+    vector<int> ans;
+    while(pq.size() > 0) {
+        ans.push_back(pq.top().second);
+        pq.pop();
+    }
+
+    sort(ans.begin(), ans.end());
+
+    return ans;
+}
