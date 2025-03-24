0892ee951f52b47dfa048ee890dca50577362feb
Ayush Pratap <ayushpratap2127@gmail.com>
2025-03-24 22:29:29 +0530

    Create merge.cpp

---
+++ Sorting/merge.cpp
@@ -0,0 +1,107 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    int n = intervals.size();
+
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>> ans;
+
+    int start = intervals[0][0];
+    int end = intervals[0][1];
+    for(int i = 1; i < n; ) {
+        while(i < n){
+            if(intervals[i][0] <= end) {
+                end = max(end, intervals[i][1]);
+            }else {
+                break;
+            }
+            i++;
+        }
+
+        if(i < n){
+            ans.push_back({start, end});
+            start = intervals[i][0];
+            end = intervals[i][1];
+        }
+
+    }
+    ans.push_back({start, end});
+
+    return ans;
+}
