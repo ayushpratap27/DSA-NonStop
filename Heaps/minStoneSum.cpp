307e6ccaaaa6cd6feebaa6b229d22179b8721399
Ayush Pratap <ayushpratap2127@gmail.com>
2025-04-06 23:49:18 +0530

    Update minStoneSum.cpp

--- Heaps/minStoneSum.cpp
+++ Heaps/minStoneSum.cpp
@@ -21,3 +21,27 @@ int minStoneSum(vector<int> &piles, int k){
 } 
 // Time Complexity: O(n log n) due to priority queue operations
 // Space Complexity: O(n) for storing the elements in the priority queue
+
+
+int minStoneSum(vector<int>& piles, int k) {
+    priority_queue<int> pq;
+    int sum = 0;
+    for(int i = 0; i < piles.size(); i++) {
+        pq.push(piles[i]);
+        sum += piles[i];
+    }
+
+    for(int i = 1; i <= k; i++) {
+        int top = pq.top();
+        pq.pop();
+
+        int remove = top / 2;
+
+        sum -= remove;
+        top -= remove;
+
+        pq.push(top);
+    }
+
+    return sum;
+}
