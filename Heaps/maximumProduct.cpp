287d4d3b82f9ee3b36c593e9456062f462d0598f
Ayush Pratap <ayushpratap2127@gmail.com>
2025-04-11 23:25:48 +0530

    Create maximumProduct.cpp

---
+++ Heaps/maximumProduct.cpp
@@ -0,0 +1,99 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int mod = 1e9 + 7;
+
+int maximumProduct(vector<int>& nums, int k) {
+    int n = nums.size();
+    priority_queue<int, vector<int>, greater<int>> pq(nums.begin(), nums.end());
+
+    while(k--){
+        int node = pq.top();
+        pq.pop();
+        node++;
+        pq.push(node);
+    }
+    
+    long long ans = 1;
+    while(!pq.empty()){
+        ans = (ans * pq.top()) % mod;
+        pq.pop();
+    }
+
+    return ans;
+}
