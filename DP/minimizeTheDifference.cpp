f9860918386842051746979e5df93e87127ada8d
Ayush Pratap <ayushpratap2127@gmail.com>
2025-04-12 18:50:31 +0530

    Create minimizeTheDifference.cpp

---
+++ DP/minimizeTheDifference.cpp
@@ -0,0 +1,100 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int solve(int i, int sum, int target, vector<vector<int>> &mat, vector<vector<int>> &dp) {
+    int n = mat.size();
+    int m = mat[0].size();
+    if (i == n) return abs(sum - target);
+    
+    if (dp[i][sum] != -1) return dp[i][sum];
+
+    int res = INT_MAX;
+    for (int val : mat[i]) {
+        res = min(res, solve(i + 1, sum + val, target, mat, dp));
+    }
+
+    return dp[i][sum] = res;
+}
+
+int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+    int n = mat.size();
+    int m = mat[0].size();
+    vector<vector<int>> dp(n, vector<int>(4901, -1));
+
+    return solve(0, 0, target, mat, dp);
+}
