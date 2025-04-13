a8fdcbbe8e29403538c11042e3e7cb6e27741768
Ayush Pratap <ayushpratap2127@gmail.com>
2025-04-13 22:00:44 +0530

    Create findPeakGrid.cpp

---
+++ Binary Search/findPeakGrid.cpp
@@ -0,0 +1,107 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+vector<int> findPeakGrid(vector<vector<int>>& mat) {
+    int n = mat.size();
+    int m = mat[0].size();
+    int low = 0, high = m - 1;
+
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        int maxRow = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(mat[i][mid] > mat[maxRow][mid]) {
+                maxRow = i;
+            }
+        }
+
+        bool left = (mid - 1 >= 0) && (mat[maxRow][mid] < mat[maxRow][mid - 1]);
+        bool right = (mid + 1 < m) && (mat[maxRow][mid] < mat[maxRow][mid + 1]);
+
+        if (!left && !right) {
+            return {maxRow, mid}; 
+        } else if (left) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;  
+        }
+    }
+
+    return {-1, -1}; 
+}
