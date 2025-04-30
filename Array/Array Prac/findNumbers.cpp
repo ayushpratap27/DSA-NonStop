226b3d0b57cdb54e5fbe521cf25c3ccfe5a1f85e
Ayush Pratap <ayushpratap2127@gmail.com>
2025-04-30 23:32:15 +0530

    Create findNumbers.cpp

---
+++ Array/Array Prac/findNumbers.cpp
@@ -0,0 +1,100 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+bool f(int num) {
+    int cnt = 0;
+    while(num){
+        cnt++;
+        num /= 10;
+    }
+    if(cnt % 2 == 0) return true;
+    return false;
+}
+
+int findNumbers(vector<int>& nums) {
+    int n = nums.size();
+    int cnt = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(f(nums[i])){
+            cnt++;
+        }
+    }
+
+    return cnt;
+}
