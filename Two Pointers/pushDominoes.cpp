b8c945692135b86b936a01e07b2e5d66b3d812cf
Ayush Pratap <ayushpratap2127@gmail.com>
2025-05-02 23:28:23 +0530

    Create pushDominoes.cpp

---
+++ Two Pointers/pushDominoes.cpp
@@ -0,0 +1,105 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+string pushDominoes(string s) {
+    s = 'L' + s + 'R';
+    string res;
+
+    int prev = 0;
+    for(int curr = 1; curr < s.size(); curr++){
+        if(s[curr] == '.'){
+            continue;
+        }
+
+        int span = curr - prev - 1;
+        if(prev > 0) 
+            res += s[prev];
+
+        if(s[prev] == s[curr]){
+            res += string(span, s[prev]);
+        }
+        else if(s[prev] == 'L' && s[curr] == 'R'){
+            res += string(span, '.');
+        }
+        else{ 
+            res += string(span / 2, 'R') + string(span % 2, '.') + string(span / 2, 'L');
+        }
+        prev = curr;
+    }
+    return res;
+}
