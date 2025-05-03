c2e1b4ad2867efb24b43135de8628221f4695aa9
Ayush Pratap <ayushpratap2127@gmail.com>
2025-05-03 23:06:44 +0530

    Create minDominoRotation.cpp

---
+++ Array/Array Prac/minDominoRotation.cpp
@@ -0,0 +1,96 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+    int res = INT_MAX;
+    for (int val = 1; val <= 6; val++) {
+        int top_swaps = 0, bottom_swaps = 0;
+        bool valid = true;
+        for (int i = 0; i < tops.size(); i++) {
+            if (tops[i] != val && bottoms[i] != val) {
+                valid = false;
+                break;
+            }
+            if (tops[i] != val) top_swaps++;
+            if (bottoms[i] != val) bottom_swaps++;
+        }
+        if (valid)
+            res = min(res, min(top_swaps, bottom_swaps));
+    }
+    return res == INT_MAX ? -1 : res;
+}
