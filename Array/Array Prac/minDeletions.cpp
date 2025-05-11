8dcf92822b8c69c40971fdf0b5f0f518863b59e9
Ayush Pratap <ayushpratap2127@gmail.com>
2025-05-11 22:20:29 +0530

    Create minDeletions.cpp

---
+++ Array/Array Prac/minDeletions.cpp
@@ -0,0 +1,104 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int minDeletion(string s, int k) {
+    unordered_map<char, int> mp;
+    for(char it : s){
+        mp[it]++;
+    }
+    
+    vector<int> freq;
+    for(auto it : mp){
+        freq.push_back(it.second);
+    }
+    
+    sort(freq.begin(), freq.end());
+    
+    int size = freq.size();
+    if(size <= k){
+        return 0;
+    }
+    
+    int cnt = 0;
+    int rem = size - k;
+    for(int i = 0; i < rem; i++) {
+        cnt += freq[i];
+    }
+    
+    return cnt;
+}
