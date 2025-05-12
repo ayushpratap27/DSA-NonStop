4a784b9a6dc28123b984cf1b645d595cc9be4b30
Ayush Pratap <ayushpratap2127@gmail.com>
2025-05-12 22:21:48 +0530

    Create findEvenNumbers.cpp

---
+++ Array/Array Prac/findEvenNumbers.cpp
@@ -0,0 +1,105 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+vector<int> findEvenNumbers(vector<int>& digits) {
+    int n = digits.size();
+    unordered_set<int> nums; 
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < n; k++) {
+                if(i == j || j == k || i == k) {
+                    continue;
+                }
+                int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                if(num >= 100 && num % 2 == 0) {
+                    nums.insert(num);
+                }
+            }
+        }
+    }
+
+    vector<int> res;
+    for(auto it : nums) {
+        res.push_back(it);
+    }
+
+    sort(res.begin(), res.end());
+    
+    return res;
+}
