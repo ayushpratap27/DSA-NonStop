181e2f330a487106914bac762fa8fbd15968296e
Ayush Pratap <ayushpratap2127@gmail.com>
2025-05-17 22:11:56 +0530

    Create wiggleSort.cpp

---
+++ Array/Array Prac/wiggleSort.cpp
@@ -0,0 +1,96 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+void wiggleSort(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> nums1(nums);
+    sort(nums1.begin(), nums1.end());
+    int i = n - 1;
+    int j = 0;
+    int k = i / 2 + 1;
+    while(i >= 0) {
+        if(i % 2 == 1) {
+            nums[i] = nums1[k];
+            k++;
+        } else {
+            nums[i] = nums1[j];
+            j++;
+        }
+        i--;
+    }
+}
