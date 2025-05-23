ac3058b32d81e0e5c78053c9c41483072b7eec20
Ayush Pratap <ayushpratap2127@gmail.com>
2025-05-23 23:03:23 +0530

    Create maximumValueSum.cpp

---
+++ DP/maximumValueSum.cpp
@@ -0,0 +1,89 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+long long maximumValueSum(vector<int>& nums, const int k, vector<vector<int>>& edges)  {
+    int n = nums.size();
+    long long dp0 = 0, dp1 = INT_MIN;
+    for(int i = 1; i <= n; i++){
+        long long x = nums[i - 1], xk = x ^ k;
+        long long dp_0 = max(x + dp0, xk + dp1);
+        dp1 = max(x + dp1, xk + dp0);
+        dp0 = dp_0;
+    }
+    return dp0;
+}
