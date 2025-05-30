a6f11fe8707b743d4c61d14cc9978657f241ced5
Ayush Pratap <ayushpratap2127@gmail.com>
2025-05-30 21:33:56 +0530

    Create closestMeetingNode.cpp

---
+++ Graphs/closestMeetingNode.cpp
@@ -0,0 +1,111 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+class Solution {
+private:
+    vector<int> getDistances(const vector<int>& edges, int start) {
+        int n = edges.size();
+        vector<int> dist(n, -1);
+        int d = 0;
+        while (start != -1 && dist[start] == -1) {
+            dist[start] = d++;
+            start = edges[start];
+        }
+        
+        return dist;
+    }
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        vector<int> dist1 = getDistances(edges, node1);
+        vector<int> dist2 = getDistances(edges, node2);
+        int result = -1;
+        int minDistance = INT_MAX;
+
+        for (int i = 0; i < edges.size(); i++) {
+            if (dist1[i] != -1 && dist2[i] != -1) {
+                int maxDist = max(dist1[i], dist2[i]);
+                if (maxDist < minDistance) {
+                    minDistance = maxDist;
+                    result = i;
+                }
+            }
+        }
+        return result;
+    }
+
+};
