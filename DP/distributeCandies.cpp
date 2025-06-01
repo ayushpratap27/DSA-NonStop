0b9ae7f7e2951af23e9cc08cfd6785e5f1dbc3c2
Ayush Pratap <ayushpratap2127@gmail.com>
2025-06-01 23:44:59 +0530

    Create distributeCandies.cpp

---
+++ DP/distributeCandies.cpp
@@ -0,0 +1,93 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+long long distributeCandies(int n, int limit) {
+    vector<vector<long long>> dp(2, vector<long long>(n + 1));
+    
+    dp[0][0] = 1;
+    for (int j = 1; j <= n; j++) {
+        dp[0][j] = dp[0][j - 1] + (j <= limit);
+    }
+
+    dp[1][0] = 1;
+    for(int j = 1; j <= n; j++) {
+        dp[1][j] += dp[1][j - 1] + (dp[1 - 1][j] - (j - limit - 1 >= 0 ? dp[1 - 1][j - limit - 1] : 0));
+    }
+
+    return dp[1][n] - (n - limit - 1 >= 0 ? dp[1][n - limit - 1] : 0);
+}
