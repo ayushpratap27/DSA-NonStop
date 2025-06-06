e925452e16168592ad7575edf03c33dc23377c46
Ayush Pratap <ayushpratap2127@gmail.com>
2025-06-06 22:52:39 +0530

    Create robotWithString.cpp

---
+++ Strings/robotWithString.cpp
@@ -0,0 +1,111 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+char l(vector<int>& freq) {
+    for (int i = 0; i < 26; i++) {
+        if (freq[i]) return 'a' + i;
+    }
+    return 'a';
+}
+
+string robotWithString(string s) {
+    stack<char> st;
+    string t = "";
+    vector<int> freq(26);
+
+    for (char ch : s) {
+        freq[ch - 'a']++;
+    }
+
+    for (char ch : s) {
+        st.push(ch);
+        freq[ch - 'a']--;
+
+        while (!st.empty() && st.top() <= l(freq)) {
+            t += st.top();
+            st.pop();
+        }
+    }
+
+    while (!st.empty()) {
+        t += st.top();
+        st.pop();
+    }
+
+    return t;
+}
