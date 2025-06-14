177cb3365c3a6e591636954a288ad2cf20294a9b
Ayush Pratap <ayushpratap2127@gmail.com>
2025-06-14 23:34:45 +0530

    Create maxVowels.cpp

---
+++ Two Pointers/maxVowels.cpp
@@ -0,0 +1,103 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+bool isVowel(char ch) {
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') return true;
+    return false;
+}
+
+int maxVowels(string s, int k) {
+    int n = s.size();
+    int i = 0, j = 0;
+    int cntVowel = 0;
+    int maxVowel = INT_MIN;
+
+    while(j < n) {
+        if(isVowel(s[j])) cntVowel++;
+
+        if(j - i + 1 == k) {
+            maxVowel = max(maxVowel, cntVowel);
+            if(isVowel(s[i])) cntVowel--;
+            i++;
+        }
+
+        j++;
+    }
+
+    return maxVowel;
+}
