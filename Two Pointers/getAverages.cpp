6b5f85e76ec615d771142e3e0d79d89d0cbb8314
Ayush Pratap <ayushpratap2127@gmail.com>
2025-06-14 23:34:42 +0530

    Create getAverages.cpp

---
+++ Two Pointers/getAverages.cpp
@@ -0,0 +1,101 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+vector<int> getAverages(vector<int>& nums, int k) {
+    int n = nums.size();
+    int i = 0, j = 0;
+    long long sum = 0;  
+
+    vector<int> ans(n, -1);
+
+    while(j < n) {
+        sum += nums[j];
+
+        if(j - i + 1 == 2 * k + 1) {
+            int mid = i + k;
+            ans[mid] = sum / (2 * k + 1);
+
+            sum -= nums[i];
+            i++;
+        }
+
+        j++;
+    }
+
+    return ans;
+}
