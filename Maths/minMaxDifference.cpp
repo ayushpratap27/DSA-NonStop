90502b3a3acf405cf23b4e7b50776d4bc3ade4ae
Ayush Pratap <ayushpratap2127@gmail.com>
2025-06-16 22:51:58 +0530

    Create minMaxDifference.cpp

---
+++ Maths/minMaxDifference.cpp
@@ -0,0 +1,101 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int minMaxDifference(int num) {
+    string str = to_string(num);
+    string ma = "";
+    string mi = "";
+    int n = str.size();
+    char ele = ' ';
+    for(int i = 0; i < n; i++){
+        if(str[i] != '9'){
+            ele = str[i];
+            break;
+        }
+    }
+    for(int i = 0; i < n; i++){
+        if(ele == str[i]) ma += '9';
+        else ma += str[i];
+    }
+    for(int i = 0; i < n; i++){
+        if(str[0] == str[i]) mi += '0';
+        else mi += str[i];
+    }
+    
+    return stoi(ma) - stoi(mi);
+}
