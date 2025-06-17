f48014f2bc895699f44b31660371559755e958f3
Ayush Pratap <ayushpratap2127@gmail.com>
2025-06-17 22:58:25 +0530

    Create countGoodArrays.cpp

---
+++ Maths/countGoodArrays.cpp
@@ -0,0 +1,122 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+const int MOD = 1e9 + 7;
+const int MX = 1e5;
+
+long long fact[MX];
+long long inv_fact[MX];
+
+class Solution {
+    long long qpow(long long x, int n) {
+        long long res = 1;
+        while (n) {
+            if (n & 1) {
+                res = res * x % MOD;
+            }
+            x = x * x % MOD;
+            n >>= 1;
+        }
+        return res;
+    }
+
+    long long comb(int n, int m) {
+        return fact[n] * inv_fact[m] % MOD * inv_fact[n - m] % MOD;
+    }
+    
+    void init() {
+        if (fact[0]) {
+            return;
+        }
+        fact[0] = 1;
+        for (int i = 1; i < MX; i++) {
+            fact[i] = fact[i - 1] * i % MOD;
+        }
+
+        inv_fact[MX - 1] = qpow(fact[MX - 1], MOD - 2);
+        for (int i = MX - 1; i; i--) {
+            inv_fact[i - 1] = inv_fact[i] * i % MOD;
+        }
+    }
+
+public:
+    int countGoodArrays(int n, int m, int k) {
+        init();
+        return comb(n - 1, k) * m % MOD * qpow(m - 1, n - k - 1) % MOD;
+    }
+};
