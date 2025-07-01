78677b37e69c4f6c51bbc323aa6e9c335acecd5b
Ayush Pratap <ayushpratap2127@gmail.com>
2025-07-01 22:43:51 +0530

    Update possibleStringCount.cpp

--- Strings/possibleStringCount.cpp
+++ Strings/possibleStringCount.cpp
@@ -92,3 +92,14 @@ int possibleStringCount(string word) {
 
     return cnt;
 }
+
+int possibleStringCount(string word) {
+    int n = word.size();
+    int cnt = 1;
+    for(int i = 1; i < n; i++) {
+        if(word[i - 1] == word[i]) {
+            cnt++;
+        }
+    }
+    return cnt;
+}
