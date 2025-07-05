96365a4fb89de0a88bbe63097038cda089ee64e0
Ayush Pratap <ayushpratap2127@gmail.com>
2025-07-05 23:05:45 +0530

    Create concatHex36.cpp

---
+++ Basics/concatHex36.cpp
@@ -0,0 +1,121 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+
+string toBase16(int n) {
+    if(n == 0) return "0";
+    string str = "";
+    while(n > 0) {
+        int rem = n % 16;
+        if(rem < 10) {
+            str += (char)(rem + '0');
+        } else {
+            str += (char)(rem - 10 + 'A');
+        }
+
+        n /= 16;
+    }
+
+    reverse(str.begin(), str.end());
+    return str;
+}
+
+string toBase36(int n) {
+    if(n == 0) return "0";
+    string str = "";
+    while(n > 0) {
+        int rem = n % 36;
+        if(rem < 10) {
+            str += (char)(rem + '0');
+        } else {
+            str += (char)(rem - 10 + 'A');
+        }
+
+        n /= 36;
+    }
+    reverse(str.begin(), str.end());
+
+    return str;
+}
+
+string concatHex36(int n) {
+    int a = n * n;
+    int b = n * n * n;
+
+    return toBase16(a) + toBase36(b);
+}
