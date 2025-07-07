23d9f094b00dbd6379beff43e945851f80b831a7
Ayush Pratap <ayushpratap2127@gmail.com>
2025-07-07 23:37:52 +0530

    Create maxEvents.cpp

---
+++ Heaps/maxEvents.cpp
@@ -0,0 +1,97 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int maxEvents(vector<vector<int>>& events) {
+    int n = events.size();
+    sort(events.begin(), events.end());
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int i = 0, day = 1;
+    int cnt = 0;
+
+    while(i < n || !pq.empty()){
+        while(!pq.empty() && pq.top() < day) pq.pop();
+        while(i < n && events[i][0] == day) pq.push(events[i++][1]);
+        if(!pq.empty()){
+            pq.pop(); 
+            cnt++;
+        }
+        day++;
+    }
+
+    return cnt;
+}
