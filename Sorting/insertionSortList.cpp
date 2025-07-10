fd7aa70f28e663f8456ad91194b20c69f261245f
Ayush Pratap <ayushpratap2127@gmail.com>
2025-07-10 23:59:38 +0530

    Update insertionSortList.cpp

--- Sorting/insertionSortList.cpp
+++ Sorting/insertionSortList.cpp
@@ -76,14 +76,13 @@
 #define ll int64_t
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
