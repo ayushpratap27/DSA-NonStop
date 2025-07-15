8055a0cc0c08e618c3ddc0eb2e8138bf0fc96597
Ayush Pratap <ayushpratap2127@gmail.com>
2025-07-15 23:01:03 +0530

    Create isValid.cpp

---
+++ Strings/isValid.cpp
@@ -0,0 +1,99 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+bool isValid(string s) {
+    int n = s.length();
+    if (n < 3) return false;
+
+    int vowels = 0, consonants = 0;
+    string vowelList = "aeiouAEIOU";
+
+    for(char c : s){
+        if(isalpha(c)) {
+            if(vowelList.find(c) != string::npos) {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        } else if (!isdigit(c)) {
+            return false; 
+        }
+    }
+
+    return vowels >= 1 && consonants >= 1;
+}
