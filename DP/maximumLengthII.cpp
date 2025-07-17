9eb89e9d4d57b9bca97e3aefac33029ffafb504e
Ayush Pratap <ayushpratap2127@gmail.com>
2025-07-17 22:38:27 +0530

    Create maximumLengthII.cpp

---
+++ DP/maximumLengthII.cpp
@@ -0,0 +1,92 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int maximumLength(vector<int>& nums, int k) {
+    vector<vector<int>> dp(k, vector<int>(k, 0));
+    int maxLength = 0;
+
+    for(int num : nums) {
+        int current_rem = num % k;
+        for(int prev_rem = 0; prev_rem < k; prev_rem++)  {
+            dp[prev_rem][current_rem] = dp[current_rem][prev_rem] + 1;
+            maxLength = max(maxLength, dp[prev_rem][current_rem]);
+        }
+    }
+
+    return maxLength;
+}
