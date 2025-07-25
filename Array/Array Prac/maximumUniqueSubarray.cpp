ce9876429f65c8fd4b834c7785cdd8dfaa4f28fa
Ayush Pratap <ayushpratap2127@gmail.com>
2025-07-25 23:08:35 +0530

    Create maximumUniqueSubarray.cpp

---
+++ Array/Array Prac/maximumUniqueSubarray.cpp
@@ -0,0 +1,97 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int maximumUniqueSubarray(vector<int>& nums) {
+    int result = 0, currentSum = 0, start = 0;
+    unordered_set<int> seen;
+
+    for (int end = 0; end < nums.size(); end++) {
+        while (seen.find(nums[end]) != seen.end()) {
+            seen.erase(nums[start]);
+            currentSum -= nums[start];
+            start++;
+        }
+
+        currentSum += nums[end];
+        seen.insert(nums[end]);
+
+        result = max(result, currentSum);
+    }
+
+    return result;
+}
