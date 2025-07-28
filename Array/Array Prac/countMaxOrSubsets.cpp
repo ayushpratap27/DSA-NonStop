0cacc74e13d18dd48adfe38acf37526b6576b9f6
Ayush Pratap <ayushpratap2127@gmail.com>
2025-07-28 23:08:07 +0530

    Create countMaxOrSubsets.cpp

---
+++ Array/Array Prac/countMaxOrSubsets.cpp
@@ -0,0 +1,107 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int countSubsetsWithTargetOR(vector<int>& nums, int target) {
+    unordered_map<int, int> dp;
+    dp[0] = 1; 
+
+    for (int num : nums) {
+        unordered_map<int, int> temp = dp;
+
+        for (const auto& [orValue, count] : dp) {
+            int newOR = orValue | num; 
+            temp[newOR] += count; 
+        }
+
+        dp = temp;
+    }
+
+    return dp[target];
+}
+
+int countMaxOrSubsets(vector<int>& nums) {
+    int maxOr = nums[0];
+    for(int i=1;i<nums.size();i++){
+        maxOr = maxOr | nums[i];
+    }
+
+    int result = countSubsetsWithTargetOR(nums, maxOr);
+
+    return result;
+
+}
