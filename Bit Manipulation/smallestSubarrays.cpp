e1f9ab31288d3ee1f0b7a9acc11f80a801f9a6d7
Ayush Pratap <ayushpratap2127@gmail.com>
2025-07-29 23:00:49 +0530

    Create smallestSubarrays.cpp

---
+++ Bit Manipulation/smallestSubarrays.cpp
@@ -0,0 +1,98 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+vector<int> smallestSubarrays(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> pos(31, -1);
+    vector<int> ans(n);
+    for(int i = n - 1; i >= 0; i--){
+        int j = i;
+        for(int bit = 0; bit < 31; bit++){
+            if(!(nums[i] & (1 << bit))) {
+                if(pos[bit] != -1) {
+                    j = max(j, pos[bit]);
+                }
+            }else {
+                pos[bit] = i;
+            }
+        }
+        ans[i] = j - i + 1;
+    }
+    
+    return ans;
+}
