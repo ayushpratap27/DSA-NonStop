c6226f661354548fd7739ec268e115eeefc1e24a
Ayush Pratap <ayushpratap2127@gmail.com>
2025-07-30 23:26:25 +0530

    Create numDifferentIntegers.cpp

---
+++ Strings/numDifferentIntegers.cpp
@@ -0,0 +1,104 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int numDifferentIntegers(string s) {
+    int n = s.size();
+    int i = 0;
+    unordered_set<string> st;
+    while(i < n){
+        if(i < n && !isdigit(s[i])){
+            i++;
+            continue;
+        }
+        string temp = "";
+        if(i < n && isdigit(s[i])){
+            while(isdigit(s[i])){
+                temp += s[i];
+                i++;
+            }
+            int j = 0;
+            while(j < temp.size() && temp[j] == '0' ){
+                j++;
+            }
+            string trimmed = temp.substr(j);
+            if(trimmed == "") trimmed = "0";
+            st.insert(trimmed);
+        }
+    }
+    return st.size();
+}
