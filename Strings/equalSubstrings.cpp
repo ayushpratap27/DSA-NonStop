d823d4fad7d8ee4f68fc9d906a2b4baf9db30adf
Ayush Pratap <ayushpratap2127@gmail.com>
2025-08-05 23:44:06 +0530

    Create equalSubstrings.cpp

---
+++ Strings/equalSubstrings.cpp
@@ -0,0 +1,97 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define ll int64_t
+using namespace std;
+
+int equalSubstring(string s, string t, int maxCost) {
+    int n = s.length();
+    int maxLen = 0;
+    int l = 0, r = 0;
+    int cost = 0;
+
+    while(r < n) {
+        cost += abs(s[r] - t[r]);
+        if(cost > maxCost) {
+            cost -= abs(s[l] - t[l]);
+            l++;
+        }
+
+        if(cost <= maxCost) maxLen = max(maxLen, r - l + 1);
+        r++;
+    }
+
+    return maxLen;
+}
